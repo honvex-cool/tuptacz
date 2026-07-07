6c33072586b98741fb27cbd6047cd92d8684dcb1
honvex-cool <jakub@oskwarek.pl>
2026-07-07 11:02:44 +0200

    Update `README.md`

--- frontend/src/Main.tsx
+++ frontend/src/Main.tsx
@@ -3,6 +3,19 @@ import { Link } from "react-router-dom";
 
 import "./Main.css"
 
+// Fix missing leaflet markers
+import L from 'leaflet';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconUrl: markerIcon,
+  iconRetinaUrl: markerIcon2x,
+  shadowUrl: markerShadow,
+});
+
 type MainProps = {
     children?: ReactNode;
     title: string;
