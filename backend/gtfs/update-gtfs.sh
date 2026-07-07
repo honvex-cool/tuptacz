5104db1250d843d756be38186653df6e2a88fa0d
honvex-cool <jakub@oskwarek.pl>
2026-07-07 09:49:11 +0200

    Update docker, vite, and nginx configs

--- backend/gtfs/update-gtfs.sh
+++ backend/gtfs/update-gtfs.sh
@@ -1,5 +1,9 @@
+echo "Downloading GTFS data"
+
 rm -r KRK/
 mkdir -p KRK/A KRK/T KRK/M
+
+echo "Downloading KRK"
 curl https://gtfs.ztp.krakow.pl/GTFS_KRK_A.zip > A.zip
 curl https://gtfs.ztp.krakow.pl/GTFS_KRK_T.zip > T.zip
 curl https://gtfs.ztp.krakow.pl/GTFS_KRK_M.zip > M.zip
